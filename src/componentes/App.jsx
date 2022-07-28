392c1377a3cab81743484a71cfec170879979989
Daniel Andrés <daniel.andres.dev@gmail.com>
2022-07-27 20:24:13 -0500

    add get episodes

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -8,13 +8,28 @@ import { useState } from "react";
 function App() {
   const [movies, setMovies] = useState([]);
   const [busqueda, setBusqueda] = useState("");
+  const [seassons, setSeassons] = useState([])
 
-  const datos = (e) => {
+  const service = 'https://api.tvmaze.com'
+
+  const datos = async (e) => {
     e.preventDefault(); //evita que se recargue la pagina
     //metodo de get
-    fetch(`https://api.tvmaze.com/search/shows?q=${busqueda}`)
-    .then((response) => response.json()).then(setMovies)
+    const data = await fetch(`${service}/search/shows?q=${busqueda}`)
+    .then((response) => response.json())
+    setMovies(data)
   };
+
+  const getEpisodes = async (movieId) => {
+    const seasons = await fetch(`${service}/shows/${movieId}/episodes`).then(r => r.json())
+    let data = []
+    for (const espisode of seasons) {
+      if (temp[espisode.season]) {
+        temp[espisode.season] = [...temp[espisode.season], espisode.name]
+      } else temp[espisode.season] = [espisode.name]
+    }
+    data
+  }
 
   const cambiarState = (e) => setBusqueda(e.target.value)
 
@@ -36,8 +51,10 @@ function App() {
       </form>
       <div>
         {movies.map((x, key) => <div key={`movie-${key}`}>
-          <img src={x.show.image?.original} />
-          <div>{x.show.name}</div>
+          <div onClick={(() => getEpisodes(x.show.id))} >
+            <img src={x.show.image?.original} />
+            <div>{x.show.name}</div>
+          </div>
         </div>)}
       </div>
     </section>
